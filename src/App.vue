0c7949000b64fa091d64133bd3396dafc03e7d5b
AlexandreGaultier <alexandre.gaultier44@gmail.com>
2024-09-24 23:37:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style>
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  background-color: #919191;
+}
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+         <div class="hero-section">
+          Hero
+         </div>
+         <div class="habits-section">
+          Habits
+         </div>
+         <div class="addictions-section">
+          Addictions
+         </div>
+         <div class="workout-section">
+          Workout
+         </div>
+         <div class="logs-section">
+          Logs
+         </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Dashboard',
+    data() {
+      return {
+      };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  };
+  </script>
+  
+  <style>
+
+  </style>
